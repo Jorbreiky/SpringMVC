06f1e99d5142c2b2ed8d006ea882e22bf66f5f34
Jorge Mañon Arroyo <jorgeitch@hotmail.com>
2018-05-20 16:51:25 -0500

    Subida del octavo video

---
+++ springmvc8/src/main/java/springmvc1/controllers/Persona.java
@@ -0,0 +1,50 @@
+package springmvc1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import springmvc1.models.PersonaDAO;
+
+@Controller
+@RequestMapping(value="/Persona")
+public class Persona {
+	
+	@RequestMapping(value="/")
+	public String index(Model model) {
+		return "home";
+	}
+	
+	@RequestMapping(value="/addGET", method=RequestMethod.GET)
+	public String addGET(Model model, @RequestParam("nombre") String nombre ) {
+		model.addAttribute("nombre",nombre);
+		return "add";
+	}
+	
+	@RequestMapping(value="/addPOST", method=RequestMethod.POST)
+	public String addPOST(Model model, @RequestParam("nombre") String nombre ) {
+		model.addAttribute("nombre",nombre);
+		return "add";
+	}
+	
+	@RequestMapping(value="/add")
+	public String add(Model model, @RequestParam("nombre") String nombre) {
+		model.addAttribute("nombre",nombre);
+		return "add";
+	}
+	
+	@RequestMapping(value="/addPersona")
+	public String addPersona(Model model, PersonaDAO persona) {
+		model.addAttribute("nombre", persona.toString());
+		return "add";
+	}
+	
+	@RequestMapping(value="/addVariable/{nombre}/{apellidos}/")
+	public String addVariable(Model model, @PathVariable String nombre, @PathVariable String apellidos) {
+		model.addAttribute("nombre", nombre+" - "+apellidos);
+		return "add";
+	}
+}
---
+++ springmvc8/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Spring MVC Basico</title>
+</head>
+<body>
+	<label>Hola Mundo desde Spring MVC</label>
+	
+	<form action="/springmvc1/Persona/addPersona" method="POST">
+		<input type="text" name="id" />
+		<input type="text" name="nombre" />
+		<input type="text" name="apellidos" />
+		<button type="submit">Enviar</button>
+	</form>
+</body>
+</html>
