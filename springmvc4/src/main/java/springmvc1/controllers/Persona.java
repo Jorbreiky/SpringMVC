dc41bdbe1ac1c1233f61896eab70e5a757768e00
Jorge Mañon Arroyo <jorgeitch@hotmail.com>
2018-05-20 13:35:13 -0500

    Subida del cuarto video

---
+++ springmvc4/src/main/java/springmvc1/controllers/Persona.java
@@ -0,0 +1,21 @@
+package springmvc1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Persona {
+	
+	@RequestMapping(value="/")
+	public String index(Model model) {
+		return "home";
+	}
+	
+	@RequestMapping(value="/add")
+	public String add(Model model) {
+		model.addAttribute("nombre","Programacion JJE");
+		return "add";
+	}
+	
+}
